bc1dbe700b048b9c37b9e68a5cd2461172cb94db
dahdi <62443126+dahdi@users.noreply.github.com>
2021-04-16 12:52:36 +0100

    Created a constructor deining the automatic properities set in previous commit and annotated the code

--- Storm.cs
+++ Storm.cs
@@ -4,11 +4,22 @@ namespace MagicalInheritance
 {
     class Storm
     {
+
+        // Automatic Properties
         public string Essence
         { get; private set; }
         public bool IsStrong
         { get; private set; }
         public string Caster
         { get; private set; }
+
+
+        // Constructor defining the automatic properties
+        public Storm(string essence, bool isStrong, string caster)
+        {
+            Essence = essence;
+            IsStrong = isStrong;
+            Caster = caster;
+        }
     }
 }
