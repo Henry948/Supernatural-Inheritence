ecca16e4af77430a4808b3c534f75b23d48555f7
dahdi <62443126+dahdi@users.noreply.github.com>
2021-04-16 12:49:26 +0100

    Created Storm class and set variables

--- Storm.cs
+++ Storm.cs
@@ -2,5 +2,13 @@ using System;
 
 namespace MagicalInheritance
 {
-
+    class Storm
+    {
+        public string Essence
+        { get; private set; }
+        public bool IsStrong
+        { get; private set; }
+        public string Caster
+        { get; private set; }
+    }
 }
