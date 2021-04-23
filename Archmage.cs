60bbb81d8f9d5985ba2b23ce743f4755db72812a
dahdi <62443126+dahdi@users.noreply.github.com>
2021-04-23 08:51:00 +0100

    Created archmage class that inherits from mage

--- Archmage.cs
+++ Archmage.cs
@@ -2,5 +2,11 @@ using System;
 
 namespace MagicalInheritance
 {
-  
+    class Archmage : Mage
+    {
+        public Archmage(string Title) : base(Title)
+        {
+            
+        }
+    }
 }
